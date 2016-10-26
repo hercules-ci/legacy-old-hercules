9b6176bb44cfc345da18e9e7e52f636f32ad2f58
Joe Hermaszewski <git@monoid.al>
2016-10-26 22:07:57 +0100

    Add opaleye-gen to environment

--- default.nix
+++ default.nix
@@ -7,14 +7,6 @@ let
               mkDerivation = expr: args.mkDerivation (expr // newAttrs);
             });
       in self: super: {
-          elm-export = overrideAttrs super.elm-export {
-            src = pkgs.fetchFromGitHub {
-              owner = "expipiplus1";
-              repo = "elm-export";
-              rev = "5c6b39f3a0ace445c85c939822d6f10007ae8f11";
-              sha256 = "0mjq2y51jb26zxc8jqfs2cyvnczj54ic1zaad45gqh84vnx2jq5p";
-            };
-          };
           servant-elm = overrideAttrs super.servant-elm {
             src = pkgs.fetchFromGitHub {
               owner = "mattjbray";
@@ -26,6 +18,74 @@ let
               base elm-export lens servant servant-foreign text interpolate mockery
             ];
             doCheck = false;
+          };
+
+          opaleye-gen = with self;
+            mkDerivation {
+              pname = "opaleye-gen";
+              version = "0.1.0.0";
+              src = pkgs.fetchFromGitHub{
+                owner = "folsen";
+                repo = "opaleye-gen";
+                rev = "35e50cde7fbab9a2e082e00c4aa09b1dd99b5c43";
+                sha256 = "1bi27wfq0zx8s3iyz049lxvnr1fbjzp6ygrdy5lqwrz58xjmk1m5";
+              };
+              isLibrary = false;
+              isExecutable = true;
+              executableHaskellDepends = [
+                base bytestring cases containers countable-inflections opaleye
+                optparse-applicative postgresql-simple product-profunctors text
+                time
+              ];
+              homepage = "https://github.com/folsen/opaleye-gen#readme";
+              description = "A lightweight program to generate Opaleye boilerplate from a database";
+              license = stdenv.lib.licenses.bsd3;
+            };
+
+          #
+          # New versions for opaleye-gen
+          #
+          product-profunctors = overrideAttrs super.product-profunctors {
+            src = pkgs.fetchFromGitHub {
+              owner = "tomjaguarpaw";
+              repo = "product-profunctors";
+              rev = "1f14ce3f495cfaac292a342e9d67c3f2f753c914";
+              sha256 = "0qchq0hky05w52wpz1b6rp3y7k6z3rs16kpab175j28nqf6h47f3";
+            };
+          };
+          opaleye = overrideAttrs super.opaleye {
+            src = pkgs.fetchFromGitHub {
+              owner = "tomjaguarpaw";
+              repo = "haskell-opaleye";
+              rev = "91ac7d0001484f8ef14d3a4dd6e15d9929f43196";
+              sha256 = "09vsyv4gk3l43nrisa2rzx2y5w3wh09ln38r3y1dkmnkjmp4jynw";
+            };
+          };
+          countable-inflections = overrideAttrs super.countable-inflections {
+            src = pkgs.fetchFromGitHub {
+              owner = "folsen";
+              repo = "countable-inflections";
+              rev = "cb2f8285d3756e4a31d6c8130f07d265f706e23b";
+              sha256 = "10s2nmyab3d0kdb12xmz904271lmcr25vn5h845hgqf6qy77bqhk";
+            };
+            libraryHaskellDepends = with self; [
+              base bytestring exceptions pcre-light text pcre-utils regex-pcre-builtin
+            ];
+          };
+          cases = overrideAttrs super.cases {
+            jailbreak = true;
+          };
+
+          #
+          # new versions for servant-elm
+          #
+          elm-export = overrideAttrs super.elm-export {
+            src = pkgs.fetchFromGitHub {
+              owner = "expipiplus1";
+              repo = "elm-export";
+              rev = "5c6b39f3a0ace445c85c939822d6f10007ae8f11";
+              sha256 = "0mjq2y51jb26zxc8jqfs2cyvnczj54ic1zaad45gqh84vnx2jq5p";
+            };
           };
         };
       };
@@ -45,4 +105,10 @@ let
 
   drv = haskellPackages.callPackage f {};
 
-in drv
+  extraEnvPackages = with haskellPackages; [opaleye-gen];
+
+  envWithExtras = pkgs.lib.overrideDerivation drv.env (attrs: {
+    buildInputs = attrs.buildInputs ++ extraEnvPackages;
+  });
+
+in drv // { env = envWithExtras; }
