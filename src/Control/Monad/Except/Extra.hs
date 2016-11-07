5d372e4723586aae0f67d1a8d3b91c89262a78fd
Joe Hermaszewski <git@monoid.al>
2016-11-07 23:09:09 +0000

    Add throwWith utility functions

--- src/Control/Monad/Except/Extra.hs
+++ src/Control/Monad/Except/Extra.hs
@@ -1,6 +1,8 @@
 module Control.Monad.Except.Extra
   ( failWith
   , failWithM
+  , throwLeftWith
+  , throwLeftWithM
   , module Control.Monad.Except
   ) where
 
@@ -11,3 +13,9 @@ failWith e = maybe (throwError e) pure
 
 failWithM :: MonadError e m => e -> m (Maybe a) -> m a
 failWithM e m = failWith e =<< m
+
+throwLeftWith :: MonadError e m => (e' -> e) -> Either e' a -> m a
+throwLeftWith f = either (throwError . f) pure
+
+throwLeftWithM :: MonadError e m => (e' -> e) -> m (Either e' a) -> m a
+throwLeftWithM f m = throwLeftWith f =<< m
