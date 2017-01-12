63d46113dfa143acc6dc71eb543fbf5cc24097ae
Domen Kožar <domen@dev.si>
2017-01-12 11:58:10 +0100

    backend/default.nix: remove upstreamed changes

--- backend/default.nix
+++ backend/default.nix
@@ -6,23 +6,14 @@ let
       let overrideAttrs = package: newAttrs: package.override (args: args // {
               mkDerivation = expr: args.mkDerivation (expr // newAttrs);
             });
-          servant-auth-src = pkgs.fetchFromGitHub {
-            owner = "plow-technologies";
-            repo = "servant-auth";
-            rev = "e37c78c153048f0e0a8518645aa76a34d2d408b4";
-            sha256 = "16c6n36wawz25q3kzfs1lq5wp0aj9vdz2algnfpc3rdpg36ynwwx";
-          };
 
       in self: super: {
-          servant-auth = self.callPackage (
-            haskellPackageGen { doFilter = false; }
-                              (servant-auth-src + "/servant-auth")
-          ) {};
-
-          servant-auth-server = self.callPackage (
-            haskellPackageGen { doFilter = false; }
-                              (servant-auth-src + "/servant-auth-server")
-          ) {};
+          servant-pandoc = overrideAttrs super.servant-pandoc {
+            jailbreak = true;
+          };
+          cases = overrideAttrs super.cases {
+            jailbreak = true;
+          };
 
           servant-elm = overrideAttrs super.servant-elm {
             version = "2016-11-08";
@@ -37,7 +28,20 @@ let
             ];
             doCheck = false;
           };
+          #
+          # New versions for servant-elm
+          #
+          elm-export = overrideAttrs super.elm-export {
+            version = "2016-11-08";
+            src = pkgs.fetchFromGitHub {
+              owner = "krisajenkins";
+              repo = "elm-export";
+              rev = "d995e32482f9704efb5d7d08568b56c518f01bd0";
+              sha256 = "0zy3m1bkg2jfcsr7ac57xk53wgpf5f44zy5rxhm2nmmq309jv7g7";
+            };
+          };
 
+          # https://github.com/folsen/opaleye-gen/issues/8
           opaleye-gen = self.callPackage (
             haskellPackageGen { doFilter = false; } (
               pkgs.fetchFromGitHub{
@@ -48,16 +52,6 @@ let
               }
             )
           ) {};
-
-          servant-pandoc = overrideAttrs super.servant-pandoc {
-            jailbreak = true;
-          };
-
-          # New versions for servant-auth
-          #
-          jose = super.jose_0_5_0_1;
-
-          #
           #
           # New versions for opaleye-gen
           #
@@ -67,43 +61,6 @@ let
               repo = "product-profunctors";
               rev = "1f14ce3f495cfaac292a342e9d67c3f2f753c914";
               sha256 = "0qchq0hky05w52wpz1b6rp3y7k6z3rs16kpab175j28nqf6h47f3";
-            };
-          };
-          opaleye = overrideAttrs super.opaleye {
-            editedCabalFile = null;
-            revision = null;
-            src = pkgs.fetchFromGitHub {
-              owner = "tomjaguarpaw";
-              repo = "haskell-opaleye";
-              rev = "c068ba9d5da49735ade354717aa12e4e6d32ef9c";
-              sha256 = "01xix0d3y1mcc291rdcsanby08964935nb8c8ahb33lsn4ch707h";
-            };
-          };
-          countable-inflections = overrideAttrs super.countable-inflections {
-            src = pkgs.fetchFromGitHub {
-              owner = "folsen";
-              repo = "countable-inflections";
-              rev = "cb2f8285d3756e4a31d6c8130f07d265f706e23b";
-              sha256 = "10s2nmyab3d0kdb12xmz904271lmcr25vn5h845hgqf6qy77bqhk";
-            };
-            libraryHaskellDepends = with self; [
-              base bytestring exceptions pcre-light text pcre-utils regex-pcre-builtin
-            ];
-          };
-          cases = overrideAttrs super.cases {
-            jailbreak = true;
-          };
-
-          #
-          # new versions for servant-elm
-          #
-          elm-export = overrideAttrs super.elm-export {
-            version = "2016-11-08";
-            src = pkgs.fetchFromGitHub {
-              owner = "krisajenkins";
-              repo = "elm-export";
-              rev = "d995e32482f9704efb5d7d08568b56c518f01bd0";
-              sha256 = "0zy3m1bkg2jfcsr7ac57xk53wgpf5f44zy5rxhm2nmmq309jv7g7";
             };
           };
         };
--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs/archive/96e1220813888f8870de6c3f323db4435f38e849.tar.gz)
+import (fetchTarball https://github.com/NixOS/nixpkgs/archive/60435691f78dc533d49bf30751e1a8328b67367a.tar.gz)
