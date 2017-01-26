60842a0837de97f2a5005bed0e701a1396d56f6e
Domen Kožar <domen@dev.si>
2017-01-26 16:04:04 +0100

    Use GHC 8.0.1 to make travis happy

--- backend/default.nix
+++ backend/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? (import ./../pkgs.nix) {} }:
 
 let
-  haskellPackages = pkgs.haskell.packages.ghc802.override{
+  haskellPackages = pkgs.haskell.packages.ghc801.override{
     overrides =
       let overrideAttrs = package: newAttrs: package.override (args: args // {
               mkDerivation = expr: args.mkDerivation (expr // newAttrs);
@@ -86,7 +86,7 @@ let
   haskellPackageGen = { doHaddock ? false, doFilter ? true }: src:
     let filteredSrc = builtins.filterSource (n: t: t != "unknown") src;
         package = pkgs.runCommand "default.nix" {} ''
-          ${pkgs.haskell.packages.ghc802.cabal2nix}/bin/cabal2nix \
+          ${pkgs.haskell.packages.ghc801.cabal2nix}/bin/cabal2nix \
             ${if doFilter then filteredSrc else src} \
             ${if doHaddock
                 then ""
