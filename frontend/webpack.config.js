62026cbff8e6b3e383ff1ffcf71e20a681fcb435
Domen Kožar <domen@dev.si>
2017-03-14 15:24:25 +0100

    frontend: allow all CORS for development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
   devServer: {
     inline: true,
     stats: 'errors-only',
-    historyApiFallback: true
+    historyApiFallback: true,
+    headers: {
+       "Access-Control-Allow-Origin": "*",
+       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
 };
