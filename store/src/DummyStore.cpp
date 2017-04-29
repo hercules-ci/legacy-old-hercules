f9b76ba86df986f7b7122454f6c97d4b95636a59
Joe Hermaszewski <git@monoid.al>
2017-04-29 20:31:05 +0100

    Experiment with having a member variable store

--- store/src/DummyStore.cpp
+++ store/src/DummyStore.cpp
@@ -12,74 +12,160 @@ namespace nix {
 
 class HerculesStore : public Store {
 public:
-  ~HerculesStore();
+  ~HerculesStore(){};
 
-  HerculesStore(const Store::Params &params, const std::string &something)
-      : Store(params), BaseStore(params){};
+  HerculesStore(const Store::Params &params,
+                std::unique_ptr<Store> underlyingStore,
+                const std::string &something)
+      : Store(params), underlyingStore(std::move(underlyingStore)){};
 
+  std::string getUri() override;
+  PathSet queryAllValidPaths() override;
+  void queryPathInfoUncached(
+      const Path &path,
+      std::function<void(std::shared_ptr<ValidPathInfo>)> success,
+      std::function<void(std::exception_ptr exc)> failure) override;
+  void queryReferrers(const Path &path, PathSet &referrers) override;
+  PathSet queryDerivationOutputs(const Path &path) override;
+  StringSet queryDerivationOutputNames(const Path &path) override;
+  Path queryPathFromHashPart(const string &hashPart) override;
+  void addToStore(const ValidPathInfo &info, const ref<std::string> &nar,
+                  bool repair, bool dontCheckSigs,
+                  std::shared_ptr<FSAccessor> accessor) override;
+  Path addToStore(const string &name, const Path &srcPath, bool recursive,
+                  HashType hashAlgo, PathFilter &filter, bool repair) override;
+  Path addTextToStore(const string &name, const string &s,
+                      const PathSet &references, bool repair) override;
+  void narFromPath(const Path &path, Sink &sink) override;
   void buildPaths(const PathSet &paths, BuildMode buildMode) override;
-  BuildResult buildDerivation(const Path & drvPath, const BasicDerivation & drv,
-      BuildMode buildMode = bmNormal) override;
+  BuildResult buildDerivation(const Path &drvPath, const BasicDerivation &drv,
+                              BuildMode buildMode) override;
   void ensurePath(const Path &path) override;
+  void addTempRoot(const Path &path) override;
+  void addIndirectRoot(const Path &path) override;
+  Roots findRoots() override;
+  void collectGarbage(const GCOptions &options, GCResults &results) override;
+  ref<FSAccessor> getFSAccessor() override;
+  void addSignatures(const Path &storePath, const StringSet &sigs) override;
+
+private:
+  [[noreturn]] void notImpl(const std::string &operation);
+
+  std::unique_ptr<Store> underlyingStore;
 };
 
-template <class BaseStore> HerculesStore<BaseStore>::~HerculesStore() {}
-
-template <class BaseStore>
-void HerculesStore<BaseStore>::buildPaths(const PathSet &paths,
-                                       BuildMode buildMode) {
-  std::cerr << "buildPaths called for" << std::endl;
-  std::for_each(paths.begin(), paths.end(), [](const Path &path) {
-    std::cerr << "  - " << path << std::endl;
-  });
-
-  BaseStore::buildPaths(paths, buildMode);
+void HerculesStore::notImpl(const std::string &operation) {
+  std::cerr << operation << std::endl;
+  throw Error(operation + " is not implemented for the Hercules Store");
 }
 
-template <class BaseStore>
-BuildResult HerculesStore<BaseStore>::buildDerivation(const Path & drvPath, const BasicDerivation & drv, BuildMode buildMode) {
-  std::cerr << "buildDerivation called for" << std::endl;
-  std::cerr << "  - " << drvPath << std::endl;
+#define NOT_IMPL                                                               \
+  do {                                                                         \
+    notImpl(__PRETTY_FUNCTION__);                                              \
+  } while (0)
 
-  return BaseStore::buildDerivation(drvPath, drv, buildMode);
-};
+std::string HerculesStore::getUri() { NOT_IMPL; }
 
-template <class BaseStore>
-void HerculesStore<BaseStore>::ensurePath(const Path &path) {
-  std::cerr << "ensurePath called for" << std::endl;
-  std::cerr << "  - " << path << std::endl;
+PathSet HerculesStore::queryAllValidPaths() { NOT_IMPL; }
 
-  BaseStore::ensurePath(path);
+void HerculesStore::queryPathInfoUncached(
+    const Path &path,
+    std::function<void(std::shared_ptr<ValidPathInfo>)> success,
+    std::function<void(std::exception_ptr exc)> failure) {
+  NOT_IMPL;
+}
+
+void HerculesStore::queryReferrers(const Path &path, PathSet &referrers) {
+  NOT_IMPL;
+}
+
+PathSet HerculesStore::queryDerivationOutputs(const Path &path) { NOT_IMPL; }
+
+StringSet HerculesStore::queryDerivationOutputNames(const Path &path) {
+  NOT_IMPL;
+}
+
+Path HerculesStore::queryPathFromHashPart(const string &hashPart) { NOT_IMPL; }
+
+void HerculesStore::addToStore(const ValidPathInfo &info,
+                               const ref<std::string> &nar, bool repair,
+                               bool dontCheckSigs,
+                               std::shared_ptr<FSAccessor> accessor) {
+  NOT_IMPL;
+}
+
+Path HerculesStore::addToStore(const string &name, const Path &srcPath,
+                               bool recursive, HashType hashAlgo,
+                               PathFilter &filter, bool repair) {
+  return underlyingStore->addToStore(name, srcPath, recursive, hashAlgo, filter, repair);
+}
+
+Path HerculesStore::addTextToStore(const string &name, const string &s,
+                                   const PathSet &references, bool repair) {
+  return underlyingStore->addTextToStore(name, s, references, repair);
+}
+
+void HerculesStore::narFromPath(const Path &path, Sink &sink) { NOT_IMPL; }
+
+void HerculesStore::buildPaths(const PathSet &paths, BuildMode buildMode) {
+  NOT_IMPL;
+}
+
+BuildResult HerculesStore::buildDerivation(const Path &drvPath,
+                                           const BasicDerivation &drv,
+                                           BuildMode buildMode) {
+  NOT_IMPL;
+}
+
+void HerculesStore::ensurePath(const Path &path) { NOT_IMPL; }
+
+void HerculesStore::addTempRoot(const Path &path) { NOT_IMPL; }
+
+void HerculesStore::addIndirectRoot(const Path &path) { NOT_IMPL; }
+
+Roots HerculesStore::findRoots() { NOT_IMPL; }
+
+void HerculesStore::collectGarbage(const GCOptions &options,
+                                   GCResults &results) {
+  NOT_IMPL;
+}
+
+ref<FSAccessor> HerculesStore::getFSAccessor() { NOT_IMPL; }
+
+void HerculesStore::addSignatures(const Path &storePath,
+                                  const StringSet &sigs) {
+  NOT_IMPL;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Register the store
 ////////////////////////////////////////////////////////////////////////////////
 
+std::unique_ptr<Store> makeUnderlyingStore(const std::string &resource,
+                                           const Store::Params &params) {
+  switch (getStoreType(resource, get(params, "state", settings.nixStateDir))) {
+  case tDaemon:
+    return std::make_unique<UDSRemoteStore>(params);
+  case tLocal:
+    return std::make_unique<LocalStore>(params);
+  default:
+    return nullptr;
+  }
+}
+
 static RegisterStoreImplementation
 regStore([](const std::string &uri,
             const Store::Params &params) -> std::shared_ptr<Store> {
-  const std::string prefix = "dummy://";
-  if (std::string(uri, 0, prefix.length()) != prefix) {
+  const std::string prefix = "hercules://";
+  const std::string protocol(uri, 0, prefix.length());
+  if (protocol != prefix) {
     return nullptr;
   }
 
   const std::string resource(uri, prefix.length());
+  std::unique_ptr<Store> underlyingStore(makeUnderlyingStore(resource, params));
 
-  switch (getStoreType(resource, get(params, "state", settings.nixStateDir))) {
-  case tDaemon:
-    std::cerr
-        << "Registering dummy store with UDSRemoteStore (daemon) underneath"
-        << std::endl;
-    return std::shared_ptr<Store>(
-        std::make_shared<HerculesStore<UDSRemoteStore>>(params, ""));
-  case tLocal:
-    std::cerr << "Registering dummy store with LocalStore underneath"
-              << std::endl;
-    return std::shared_ptr<Store>(
-        std::make_shared<HerculesStore<LocalStore>>(params, ""));
-  default:
-    return nullptr;
-  }
+  return std::shared_ptr<Store>(
+      std::make_shared<HerculesStore>(params, std::move(underlyingStore), ""));
 });
 };
